906543ad165d04de6335cd9feb3acfdb69a97174
Pomegranate Liam <pomegran@su-d5.msk.21-school.ru>
2022-11-17 19:15:24 +0300

    files removed

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#define pi 3.14159265358979323846
 
-int s21_abs(int x);
-long double s21_acos(double x);
-long double s21_asin(double x);
-long double s21_atan(double x);
-long double s21_ceil(double x);
+long double s21_fabs(double x) { return (x > 0) ? x: -x; };
+int s21_abs(int x) { return (int)s21_fabs(x); }
+
 long double s21_cos(double x);
+long double s21_acos(double x);
+long double s21_sin(double x);
+long double s21_asin(double x);
+long double s21_tan(double x);
+long double s21_atan(double x);
+
+long double s21_ceil(double x); //
+long double s21_floor(double x); // 
+
 long double s21_exp(double x);
-long double s21_fabs(double x);
-long double s21_floor(double x);
 long double s21_fmod(double x, double y);
 long double s21_log(double x);
 long double s21_pow(double base, double exp);
-long double s21_sin(double x);
 long double s21_sqrt(double x);
-long double s21_tan(double x);
