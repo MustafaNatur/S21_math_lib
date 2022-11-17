2954c45a62c679de765d43cc624104dfde2ee421
Pomegranate Liam <pomegran@su-d5.msk.21-school.ru>
2022-11-17 17:37:19 +0300

    start

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,2 +1,9 @@
 #include "s21_math.h"
 
+int main() {
+    int num1 = -5;
+    int num2 = 4;
+    printf("%d %d", s21_abs(num1), s21_abs(num2));
+
+    return 0;
+}
--- src/s21_math_lib.c
+++ src/s21_math_lib.c
@@ -1,1 +1,17 @@
 #include "s21_math.h"
+
+int s21_abs(int x) { return (x > 0) ? x: -x; }
+long double s21_acos(double x);
+long double s21_asin(double x);
+long double s21_atan(double x);
+long double s21_ceil(double x);
+long double s21_cos(double x);
+long double s21_exp(double x);
+long double s21_fabs(double x);
+long double s21_floor(double x);
+long double s21_fmod(double x, double y);
+long double s21_log(double x);
+long double s21_pow(double base, double exp);
+long double s21_sin(double x);
+long double s21_sqrt(double x);
+long double s21_tan(double x);
