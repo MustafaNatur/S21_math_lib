f6552249371d08b41ff2c1699e8b60c7ab5b46bf
Loralee Chong <loraleec@mi-f2.msk.21-school.ru>
2022-11-20 20:16:23 +0300

    sqrt+facktorial

--- src/s21_math_loraleec.c
+++ src/s21_math_loraleec.c
@@ -4,10 +4,7 @@
 #include "s21_math.h"
 
 int main() {
-  double x = -12.124309423;
-  // int x = -14;
-  //   double y = 3;
-  // printf("%Lf", s21_abs(-12.12334124323432));
-  printf("%d", s21_abs(x));
+  // printf("%Lf", s21_sqrt(-4));
+  s21_sqrt(-4);
   return 0;
 }
