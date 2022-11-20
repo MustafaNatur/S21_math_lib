48e2fdfc87eead18f8aba4cab45438b18987fee8
Loralee Chong <loraleec@mi-f2.msk.21-school.ru>
2022-11-20 16:24:36 +0300

    v1.0

---
+++ src/s21_math_loraleec.c
@@ -0,0 +1,11 @@
+// #include "s21_math_loraleec.h"
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  //   double x = 12;
+  //   double y = 3;
+  //   printf("%lf", fmod(12, 0));
+  fmod(12, 0);
+  return 0;
+}
