0f6cf3241cad0b2b9cd30fbf94446db9e843b2aa
Pomegranate Liam <pomegran@su-b2.msk.21-school.ru>
2022-11-23 21:03:47 +0300

    sin

--- src/s21_factorial.c
+++ src/s21_factorial.c
@@ -11,3 +11,28 @@ long double s21_factorial(int x) {
   }
   return res;
 }
+
+
+
+long double ProdTree(int l, int r)
+{
+    if (l > r)
+        return 1;
+    if (l == r)
+         return l;
+    if (r - l == 1)
+        return (long double)l * r;
+    int m = (l + r) / 2;
+    return ProdTree(l, m) * ProdTree(m + 1, r);
+}
+        
+long double FactTree(int n)
+{
+    if (n < 0)
+        return 0;
+    if (n == 0)
+        return 1;
+    if (n == 1 || n == 2)
+        return n;
+    return ProdTree(2, n);
+}     
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,25 +1,27 @@
+#include <stdio.h>
+
 #define S21_EPS 1e-9
 #define S21_PI 3.1415926535897932
 #define S21_EXP 2.7182818284590452
 #define S21_MAX 1e10
 
-long double s21_fabs(double x); // 
-int s21_abs(int x); // 
+int s21_abs(int x);
 
-long double s21_cos(double x); 
 long double s21_acos(double x);
-long double s21_sin(double x);
 long double s21_asin(double x);
-long double s21_tan(double x);
 long double s21_atan(double x);
-
-long double s21_ceil(double x); //
-long double s21_floor(double x); // 
-
-long double s21_log(double x);
-long double s21_exp(double x); 
+long double s21_ceil(double x);
+long double s21_cos(double x);
+long double s21_exp(double x);
+long double s21_fabs(double x);
+long double s21_floor(double x);
 long double s21_fmod(double x, double y);
+long double s21_log(double x);
 long double s21_pow(double base, double exp);
-
+long double s21_sin(double x);
 long double s21_sqrt(double x);
+long double s21_tan(double x);
 long double s21_factorial(int x);
+
+long double ProdTree(int l, int r);
+long double FactTree(int n);
