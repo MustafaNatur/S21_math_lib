f117b1ad7c552e03451161a92515e83aa4375c8a
Bettina Gevin <bettinag@su-e5.msk.21-school.ru>
2022-11-27 15:04:47 +0300

    bettinag: tried to fix make file

--- src/s21_math.h
+++ src/s21_math.h
@@ -15,33 +15,3 @@ long double s21_pow(double base, double exp);
 long double s21_sin(double x);
 long double s21_sqrt(double x);
 long double s21_tan(double x);
-
-long double s21_ceil(double x) {
-  if ((int)x != x) {
-    x = (int)x + 1;
-  }
-  return x;
-}
-
-long double s21_floor(double x) {
-  if ((int)x != x) {
-    x = (int)x - 1;
-  }
-  return x;
-}
-
-long double s21_fmod(double x, double y) {
-  while (x > y) {
-    x -= y;
-  }
-  return x;
-}
-
-int s21_abs(int x) { return (int)s21_fabs(x); }
-
-long double s21_fabs(double x) {
-  if (x < 0) {
-    x *= -1;
-  }
-  return x;
-}
