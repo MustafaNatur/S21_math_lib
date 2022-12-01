08903b345ca5e1391589d6d0cdb07899d348be01
Loralee Chong <loraleec@su-d6.msk.21-school.ru>
2022-12-01 20:44:56 +0300

    loraleec : main header

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define S21_EPS 1e-9
+#define S21_EPS 1e-100
 #define S21_PI 3.1415926535897932
 #define S21_EXP 2.7182818284590452
-#define S21_MAX 1e10
+#define S21_MAX 1e100
 
 int s21_abs(int x);
 long double s21_acos(double x);
