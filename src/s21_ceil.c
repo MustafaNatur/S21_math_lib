981284ee99790c527765d75c0f546aa0eb265a9a
Loralee Chong <loraleec@mi-c4.msk.21-school.ru>
2022-12-04 14:00:25 +0300

    loraleec : Rebuild directories

--- src/s21_ceil.c
+++
@@ -1,9 +0,0 @@
-#include "s21_math.h"
-
-long double s21_ceil(double x) {
-  long double ceil_res = (long long int)x;
-  if (ceil_res > 0 && x != ceil_res) {
-    ceil_res += 1;
-  }
-  return ceil_res;
-}
--- src/s21_fabs.c
+++
@@ -1,8 +0,0 @@
-#include "s21_math.h"
-
-long double s21_fabs(double x) {
-  if (x < 0) {
-    x *= -1;
-  }
-  return x;
-}
