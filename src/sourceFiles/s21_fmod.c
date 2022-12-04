6ac5ce99d88ac6bbc07e72576e942ef7316415de
Loralee Chong <loraleec@mi-c4.msk.21-school.ru>
2022-12-04 17:29:48 +0300

    loraleec : fmod tests and changes in program

--- src/sourceFiles/s21_fmod.c
+++ src/sourceFiles/s21_fmod.c
@@ -1,8 +1,14 @@
 #include "s21_math.h"
 
 long double s21_fmod(double x, double y) {
-  while (x > y) {
-    x -= y;
+  if (y > 0) {
+    while (x > y) {
+      x -= y;
+    }
+  } else if (y < 0) {
+    x = 0;
+  } else if (y == 0) {
+    x = 0.0 / 0.0;
   }
   return x;
 }
