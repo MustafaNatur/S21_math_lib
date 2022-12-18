da76f51e78747f0534788c37e5415c43a0b6472d
Loralee Chong <loraleec@su-d6.msk.21-school.ru>
2022-12-18 18:26:24 +0300

    loraleec : forgot about debug

--- src/sourceFiles/s21_cos.c
+++ src/sourceFiles/s21_cos.c
@@ -4,7 +4,6 @@ long double s21_cos(double x) {
   long double cos = 0;
   for (int i = 0; i < 100; i++) {
     cos += s21_pow((-1), i) * s21_pow(x, 2 * i) / s21_factorial(2 * i);
-    printf("%Lf\n", s21_pow((-1), i));
   }
   return cos;
 }
