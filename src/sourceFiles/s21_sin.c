3cec8215780c374eef891d5283475e2fe63a201d
Loralee Chong <loraleec@su-d6.msk.21-school.ru>
2022-12-25 11:09:51 +0300

    loraleec : cos and sin

--- src/sourceFiles/s21_sin.c
+++ src/sourceFiles/s21_sin.c
@@ -1,15 +1,15 @@
 #include "s21_math.h"
 
-long double s21_cos(double x) {
+long double s21_sin(double x) {
   long double result = 0;
   int sign = 1;
   double xx = x * x;
-  double pw = 1;
+  double pw = x;
   double factorial = 1.0;
   for (int i = 1; i < 25; i += 2) {
     factorial /= i;
     result += sign * pw * factorial;
-    factorial /= (2 * i);
+    factorial /= (i + 1);
     sign = -sign;
     pw *= xx;
   }
